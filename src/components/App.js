9093525ec521c0d4148ec5e1fa440075e10f4797
Anurag Joshi <anuragj990@gmail.com>
2024-12-26 18:33:28 +0530

    4th try

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
-import { Parent } from "./Parent";
+import { TodoList } from "./TodoList";
 
 const App = () => {
-  return (
-    <div>
-        <Parent/>
-    </div>
-  )
+  const [todos, setTodos]=useState([{task: 'Learn React',complete: false},{task: 'Build a react app',complete: false},{task: 'Deploy the React App',complete: false}]);
+  
+      const handleComplete=(itemvalue)=>
+      {
+         
+          let newtodolist=todos.map((item)=> {if(item.task===itemvalue){return {task: item.task, complete: true}} else{return item}});
+  
+          setTodos(newtodolist);
+      }
+  
+  
+    return (
+      <div>
+      <h1 style={{textAlign: 'center',}}>Parent Component</h1>
+      <TodoList todos={todos} handleComplete={handleComplete}/>
+      </div>
+    )
 }
 
 export default App
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const Child = ({todos, handleComplete}) => {
+export const TodoList = ({todos, handleComplete}) => {
 
 
 
